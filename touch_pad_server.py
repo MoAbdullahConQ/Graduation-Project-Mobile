ea66bd6f8b0d3738bee28ebc4a79c8acb12d17b5
mhmed <mohamed553231@gmail.com>
2025-04-12 23:13:34 +0200

    setting mouse speed and time

--- touch_pad_server.py
+++ touch_pad_server.py
@@ -7,6 +7,17 @@ app=Flask(__name__)
 socketio=SocketIO(app,cors_allowed_origins="*")
 
 
+# mouse speed setting
+MOUSE_SPEED = 2 
+
+# last move time
+last_move_time = time.time()
+
+# Minimum time between movements (in seconds)
+MIN_MOVE_INTERVAL = 0.01  # 10 milliseconds
+
+
+
 
 if __name__=="__main__":
     print("Starting Mouse Server...")
